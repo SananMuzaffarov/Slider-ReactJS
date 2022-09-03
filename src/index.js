e7808fa38338d25c70586a5cda15a1c84b462f6d
SananM <smuzefferov1@gmail.com>
2022-09-04 00:48:24 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+const App = () => {
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+    return(
+        <div className='container'>
+            <Slider {...settings}>
+            {slideData.map(item => (
+                <div className='card'>
+                <div className='cardTitle'>
+                    <h1>{item.fname}</h1>
+                    <h2>{item.lname}</h2>
+                </div>
+                <div className='cardContent'>
+                    <p>{item.age}</p>
+                </div>
+            </div>
+            ))}
+            </Slider>
+        </div>
+    );
+}
+
+const slideData = [
+    {
+        fname: 'Jack',
+        lname: 'Harrison',
+        age: '19'
+    },
+    {
+        fname: 'Jamie',
+        lname: 'Harrison',
+        age: '22'
+    },
+    {
+        fname: 'Harry',
+        lname: 'Harrison',
+        age: '26'
+    },
+    {
+        fname: 'James',
+        lname: 'Harrison',
+        age: '24'
+    },
+    {
+        fname: 'Jacob',
+        lname: 'Harrison',
+        age: '32'
+    },
+    {
+        fname: 'Patrick',
+        lname: 'Harrison',
+        age: '40'
+    }
+
+]
+root.render(<App />);
